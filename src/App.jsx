4d42fd6affc5f1f9ec53804496aca680e5eefbc3
Leo McClellan <mcclellan2@usf.edu>
2024-12-05 19:27:58 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import React from 'react';
+import BarChart from './BarChart';
+
+function App() {
+
+  return (
+    <>
+        <div>
+            <h1>My Simple Chart</h1>
+            <BarChart />
+        </div>
+    </>
+  )
+}
+
+export default App
